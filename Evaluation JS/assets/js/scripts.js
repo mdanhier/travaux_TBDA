87a5b8729882944f5b817d1f70d1c3a8cac5f869
Morgan <danhier.morgan@gmail.com>
2023-02-15 09:05:59 +0100

    Exercice 4 terminé

--- Evaluation JS/assets/js/scripts.js
+++ Evaluation JS/assets/js/scripts.js
@@ -24,8 +24,10 @@ function testNumbersMany(...nbArr) {
     })
 }
 
-function showDivEx(msg, ex) {
-    document.getElementById('divEx' + ex).innerHTML = msg;
+function showDivEx(msg, ex, replace) {
+    // si replace est à true on remplace le contenu de la div d'exercice (permet d'accumuler ou non plusieurs textes pour un seul exercice)
+    if (replace) document.getElementById('divEx' + ex).innerHTML = msg;
+    else document.getElementById('divEx' + ex).innerHTML += (msg + "<br>");
 }
 
 function ex1_1() {
@@ -59,7 +61,7 @@ function ex1_1() {
         }
         msg += "Cela fait " + young + " jeune(s), " + adult + " adulte(s) et " + old + " vieille(s) personne(s).<br>"
         msg += "En tout, il y a " + (young + adult + old) + " personnes.";
-        showDivEx(msg, 1);
+        showDivEx(msg, 1, true);
     }
     catch (err) {
         alert(err.message);
@@ -79,32 +81,42 @@ function ex2_1() {
     let nb = prompt("De quel nombre voulez-vous voir la table de multiplication ?");
     let mul = prompt("Jusqu'à quel multiple voulez-vous aller ?");
     try {
-        tableMult(nb, mul);
+        tableMult(nb, mul, true);
     }
     catch (err) {
         alert(err.message);
     }
 }
 
-function ex3_1() {
-    let name = "";
-    var nameArr = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-    try {
-        name = prompt("Veuillez entrer un prénom à retirer.")
-        if (testLetters(name)) throw new Error("Merci de rentrer un prénom correct.");
-        if (nameArr.indexOf(name) >= 0) {
-            nameArr.splice(nameArr.indexOf(name), 1);
-            nameArr.push("");
-            showDivEx(JSON.stringify(nameArr), 3);
+let nameArr = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+function ex3_1(reset) {
+    // permet de réinitialiser le tableau pour recommencer l'exercice
+    if (reset) {
+        nameArr = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"]
+        showDivEx("", 3, true);
+    }
+    else {
+        let name = "";
+        try {
+            name = prompt("Veuillez entrer un prénom à retirer.")
+            // on vérifie que le prénom est un prénom valide
+            if (testLetters(name)) throw new Error("Merci de rentrer un prénom correct.");
+            // si on trouve le prénom dans le tableau, on le retire et on insère une donnée vide à la fin du tableau
+            if (nameArr.indexOf(name) >= 0) {
+                nameArr.splice(nameArr.indexOf(name), 1);
+                nameArr.push("");
+                showDivEx(JSON.stringify(nameArr), 3, false);
+            }
+            else throw new Error("Le prénom n'a pas été trouvé.");
         }
-        else throw new Error("Le prénom n'a pas été trouvé.");
-    }
-    catch (err) {
-        alert(err.message);
+        catch (err) {
+            alert(err.message);
+        }
     }
 }
 
-function ex4_1() {
+function ex4_1(test, reset) {
+    // fonction de calcul du prix selon le PU et la quantité
     function calcPrice(unitprice, qty) {
         let tempPrice;
         let discountPercent = 0;
@@ -112,6 +124,7 @@ function ex4_1() {
         let shippingPercent = 2;
         let shippingPrice
         let priceToPay;
+        unitprice = Number(unitprice).toFixed(2);
         msg = "Prix : " + unitprice + "€ / Quantité : " + qty + " / ";
         // on calcule le total du prix des articles selon leur quantité
         tempPrice = Number(unitprice * qty).toFixed(2);
@@ -120,33 +133,46 @@ function ex4_1() {
         if (tempPrice >= 100 && tempPrice <= 200) discountPercent = 5;
         else if (tempPrice > 200) discountPercent = 10;
         discountPrice = Number(tempPrice * (discountPercent / 100)).toFixed(2);
-        tempPrice -= discountPrice;
+        tempPrice = Number(tempPrice - discountPrice).toFixed(2);
         msg += "Remise : " + discountPercent + "% (-" + discountPrice + "€) soit " + tempPrice + "€";
         // on calcule les frais de port selon le prix après remise
-        if (tempPrice >= 500) shippingPrice = 0;
+        if (tempPrice >= 500) {
+            shippingPrice = Number(0).toFixed(2)
+            msg += " et frais de port " + shippingPercent + "% soit " + shippingPrice + "€";
+        }
         else {
             shippingPrice = Number(tempPrice * (shippingPercent / 100)).toFixed(2);
-            if (shippingPrice < 6) shippingPrice = 6;
-            msg += " et frais de port " + shippingPercent + "% soit " + shippingPrice + "€ (minimum 6€)";
+            if (shippingPrice < 6) shippingPrice = Number(6).toFixed(2);
+            msg += " et frais de port " + shippingPercent + "% soit " + shippingPrice + "€ (a minima 6€)";
         }
         // on calcule le prix à payer
         priceToPay = Number(+tempPrice + +shippingPrice).toFixed(2);
         msg += ". Prix à payer : " + tempPrice + " + " + shippingPrice + " = " + priceToPay + "€";
         return msg;
     }
-    try {
-        //unitprice = prompt("Veuillez entrer un prix unitaire.")
-        //qty = prompt("Veuillez entrer une quantité.")
-        /* if (testNumbersMany(unitprice, qty) || unitprice < 0 || qty < 1) throw new Error("Merci de rentrer des nombres corrects.");
-        else {
-            calcPrice(unitprice, qty);
-        } */
-        showDivEx(calcPrice(600, 1), 4)
-        showDivEx(calcPrice(501, 1), 4)
-        showDivEx(calcPrice(100, 2), 4)
-        showDivEx(calcPrice(3, 1), 4)
+    // permet de réinitialiser le tableau pour recommencer l'exercice
+    if (reset) {
+        showDivEx("", 4, true);
     }
-    catch (err) {
-        alert(err.message);
+    else {
+        try {
+            if (test) {
+                showDivEx(calcPrice(600, 1), 4, false)
+                showDivEx(calcPrice(501, 1), 4, false)
+                showDivEx(calcPrice(100, 2), 4, false)
+                showDivEx(calcPrice(3, 1), 4, false)
+            }
+            else {
+                unitprice = prompt("Veuillez entrer un prix unitaire.")
+                qty = prompt("Veuillez entrer une quantité.")
+                if (testNumbersMany(unitprice, qty) || unitprice < 0 || qty < 1) throw new Error("Merci de rentrer des nombres corrects.");
+                else {
+                    showDivEx(calcPrice(unitprice, qty), 4, false);
+                }
+            }
+        }
+        catch (err) {
+            alert(err.message);
+        }
     }
 }
